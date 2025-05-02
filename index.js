6f5db7f95b791024fdb7fdc7723e3d444370b895
Karina Chernobai <karina.chernobai@gmail.com>
2025-05-02 16:21:58 +0200

    Add tultips

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+class Season {
+    constructor(name, sounds) {
+        this.name = name;
+        this.sounds = sounds;
+    }
+};
+
+class Sound {
+    constructor(src, descr) {
+        this.src = src;
+        this.descr = descr;
+    }
+}
+
+const seasons = [
+    new Season("spring", [new Sound("birds", "Morning Birds"), new Sound("brook", "Babbling Brook"), new Sound("rain", "Spring Rain")]),
+    new Season("summer", [new Sound("ocean", "Ocean Waves"), new Sound("crickets", "Summer Night"), new Sound("breeze", "Gentle Breeze")]),
+    new Season("autumn", [new Sound("leaves", "Rustling Leaves"), new Sound("thunder", "Distant Thunder"), new Sound("rainfall", "Autumn Rainfall")]),
+    new Season("winter", [new Sound("fireplace", "Crackling Fire"), new Sound("snowfall", "Falling Snow"), new Sound("snowcrunch", "Snow Crunch")]),
+];
+let currSeasonInx = 0;
+
 document.addEventListener('DOMContentLoaded', function () {
-    const seasonContainer = document.querySelectorAll('.season')[0];
-    const seasons = ["spring", "summer", "autumn", "winter"];
-    let currSeasonInx = 0;
+    const seasonContainer = document.querySelector('.season');
+    const soundBtns = document.querySelectorAll('.sound-btn');
 
     function nextSeason(){
         if (currSeasonInx === seasons.length - 1) setActiveSeason(0);
@@ -21,8 +42,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function setActiveSeason(nextSeasonInx) {
-        seasonContainer.classList.remove(seasons[currSeasonInx]);
-        seasonContainer.classList.add(seasons[nextSeasonInx]);
+        seasonContainer.classList.remove(seasons[currSeasonInx].name);
+        seasonContainer.classList.add(seasons[nextSeasonInx].name);
+        const sounds = seasons[nextSeasonInx].sounds;
+        if (sounds.length !== soundBtns.length) throw new Error(`The length of the season sounds array is ${sounds.length} while there are ${soundBtns.length} on the page.`);
+        for (let index = 0; index < sounds.length; index++) {
+            const btn = soundBtns[index];
+            const sound = sounds[index];
+            btn.setAttribute("data-sound", sound.src);
+            btn.textContent = sound.descr;
+        }
         currSeasonInx = nextSeasonInx;
     };
 
